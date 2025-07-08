420673f3a38783a992f67ebb50bf115bab68d9c2
DrStrong177013 <149897720+DrStrong177013@users.noreply.github.com>
2025-07-09 04:36:36 +0700

    chapterscreen dev

--- Assets/Scripts/ChapterScreen/LevelButton.cs
+++ Assets/Scripts/ChapterScreen/LevelButton.cs
@@ -70,4 +70,50 @@ public class LevelButton : MonoBehaviour, IPointerClickHandler
         }
     }
 
+    void UnlockNextLevel()
+    {
+        int nextLevel = levelNumber + 1;
+        GameObject nextLevelObject = GameObject.Find($"Level {nextLevel}");
+        if (nextLevelObject != null)
+        {
+            LevelButton nextLevelButton = nextLevelObject.GetComponent<LevelButton>();
+            if (nextLevelButton != null)
+            {
+                nextLevelButton.levelUnlocked = true;
+                if (!fullCleared)
+                    Debug.Log($"Next level unlock: level {nextLevel}");
+                else
+                {
+                    Debug.Log("Congratulations you have completed all the challenges in this level");
+                }
+                nextLevelButton.UpdateLevelUI(nextLevel, true, false); // Update the UI for the next level
+            }
+        }
+    }
+
+
+    public void UpdateLevelUI(int levelNumber, bool isLevelUnlocked, bool isLevelCompleted)
+    {
+        GameObject level = GameObject.Find($"Level {levelNumber}");
+
+        level.SetActive(isLevelUnlocked);
+
+        GameObject levelTextInactive = level.transform.Find("LevelText/Level Inactive").gameObject;
+        GameObject levelTextActive = level.transform.Find("LevelText/Level Active").gameObject;
+        levelTextInactive.GetComponent<Image>().color = new Color(0.7f, 0f, 0.5f, 1f);
+        levelTextInactive.SetActive(!isLevelCompleted);
+        levelTextActive.SetActive(isLevelCompleted);
+    }
+
+    void SetStarsForLevel(int levelNumber, int starsAchieved)
+    {
+        GameObject level = GameObject.Find($"Level {levelNumber}");
+        var starContainer = level.transform.Find("Stars");
+
+        for (int i = 0; i < starContainer.childCount; i++)
+        {
+            Transform star = starContainer.GetChild(i);
+            star.gameObject.SetActive(i < starsAchieved);
+        }
+    }
 }
