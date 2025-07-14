7072e3d094800e448a8dc8a8d205afcfb5df9b67
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2025-07-14 11:18:53 +0700

    sound

--- Assets/Scripts/ChapterScreen/LevelButton.cs
+++ Assets/Scripts/ChapterScreen/LevelButton.cs
@@ -215,6 +215,7 @@ public class LevelButton : MonoBehaviour, IPointerClickHandler
         }
 
 
+        GameEvents.OpenLevel(stageNumber, levelNumber);
     }
 
     #region Star and Level Management
